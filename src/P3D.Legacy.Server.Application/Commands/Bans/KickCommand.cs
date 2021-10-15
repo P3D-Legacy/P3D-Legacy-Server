6f9fe2e71be3dd6c75f88a3c1f9264b0568a96a1
Aragas <personal@aragas.org>
2021-10-15 09:52:33 +0300

    Cleanup

--- src/P3D.Legacy.Server.Application/Commands/Bans/KickCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-using P3D.Legacy.Server.Abstractions;
-
-namespace P3D.Legacy.Server.Application.Commands.Bans
-{
-    public record KickCommand(IPlayer Player, string Reason) : IRequest<CommandResult>;
-}
--- src/P3D.Legacy.Server.Application/Commands/Bans/UnbanCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-using P3D.Legacy.Common;
-
-namespace P3D.Legacy.Server.Application.Commands.Bans
-{
-    public record UnbanCommand(GameJoltId Id) : IRequest<CommandResult>;
-}
