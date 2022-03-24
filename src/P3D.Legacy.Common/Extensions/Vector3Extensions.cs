964a3f91098a9a44343d5d15cbdba5bac46d6842
aragas <va_mikhaylov@head-point.ru>
2022-03-24 14:38:02 +0300

    Faster Vector3.FromP3DString, fixed parsing

--- src/P3D.Legacy.Common/Extensions/Vector3Extensions.cs
+++ src/P3D.Legacy.Common/Extensions/Vector3Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Numerics;
 using System.Text;
 
@@ -6,21 +7,35 @@ namespace P3D.Legacy.Common.Extensions
 {
     public static class Vector3Extensions
     {
-        public static Vector3 FromP3DString(string @string, char gameSeparator)
+        public static Vector3 FromP3DString(ReadOnlySpan<char> chars, char gameSeparator)
         {
             var numberFormat = new NumberFormatInfo { NumberDecimalSeparator = gameSeparator.ToString() };
 
-            var data = @string.Split('|');
+            Span<int> indices = stackalloc int[2];
+            var spanIdx = 0;
+            var span = chars;
+            while (span.IndexOf('|') is var idx && idx != -1)
+            {
+                if (spanIdx > 2)
+                    return Vector3.Zero;
 
-            if (data.Length != 3)
+                indices[spanIdx] = spanIdx != 0 ? idx + indices[spanIdx - 1] + 1 : idx;
+                span = span.Slice(idx + 1);
+                spanIdx++;
+            }
+            if (spanIdx != 2)
                 return Vector3.Zero;
 
-            var xb = float.TryParse(data[0], NumberStyles.AllowDecimalPoint, numberFormat, out var x);
-            var yb = float.TryParse(data[1], NumberStyles.AllowDecimalPoint, numberFormat, out var y);
-            var zb = float.TryParse(data[2], NumberStyles.AllowDecimalPoint, numberFormat, out var z);
+            var xs = chars.Slice(0, indices[0]);
+            var ys = chars.Slice(indices[0] + 1, indices[1] - indices[0] - 1);
+            var zs = chars.Slice(indices[1] + 1);
+
+            var xb = float.TryParse(xs, NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign, numberFormat, out var x);
+            var yb = float.TryParse(ys, NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign, numberFormat, out var y);
+            var zb = float.TryParse(zs, NumberStyles.AllowDecimalPoint | NumberStyles.AllowLeadingSign, numberFormat, out var z);
 
             if (xb && yb && zb)
-                return new Vector3(x * 1000 / 1000, y * 1000 / 1000, z * 1000 / 1000);
+                return new Vector3(x, y, z);
             else
                 return Vector3.Zero;
         }
@@ -29,11 +44,11 @@ namespace P3D.Legacy.Common.Extensions
             var numberFormat = new NumberFormatInfo { NumberDecimalSeparator = gameSeparator.ToString() };
 
             var data = new StringBuilder();
-            data.Append((vector3.X * 1000 / 1000).ToString(numberFormat));
+            data.Append(vector3.X.ToString(numberFormat));
             data.Append('|');
-            data.Append((vector3.Y * 1000 / 1000).ToString(numberFormat));
+            data.Append(vector3.Y.ToString(numberFormat));
             data.Append('|');
-            data.Append((vector3.Z * 1000 / 1000).ToString(numberFormat));
+            data.Append(vector3.Z.ToString(numberFormat));
 
             return data.ToString();
         }
