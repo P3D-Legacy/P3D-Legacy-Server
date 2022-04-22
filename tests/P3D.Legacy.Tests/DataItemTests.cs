76052fbc6272dce730e2e0f1d6018c1710adb198
Aragas <personal@aragas.org>
2022-04-22 18:12:45 +0300

    Dep update
    Fix

--- tests/P3D.Legacy.Tests/DataItemTests.cs
+++ tests/P3D.Legacy.Tests/DataItemTests.cs
@@ -19,7 +19,7 @@ namespace P3D.Legacy.Tests
             public string Name { get => DataItemStorage.Get(0); set => DataItemStorage.Set(0, value); }
             public bool IsCorrect { get => DataItemStorage.GetBool(1); set => DataItemStorage.Set(1, value); }
             public ulong IdLong { get => DataItemStorage.GetUInt64(2); set => DataItemStorage.Set(2, value); }
-            public char Char { get => DataItemStorage.GetChar(3); set => DataItemStorage.Set(3, value); }
+            public char? Char { get => DataItemStorage.GetChar(3); set => DataItemStorage.Set(3, value); }
             public int IdInt { get => DataItemStorage.GetInt32(4); set => DataItemStorage.Set(4, value); }
 
             private string Position { get => DataItemStorage.Get(5); set => DataItemStorage.Set(5, value); }
