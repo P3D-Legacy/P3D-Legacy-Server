a2a1a14570fb93dc4222068fdba997efd42749e4
Vitalii Mikhailov <personal@aragas.org>
2023-06-07 19:03:15 +0300

    Added main span for compensation service

--- src/P3D.Legacy.Server.Client.P3D/Services/P3DPlayerMovementCompensationService.cs
+++ src/P3D.Legacy.Server.Client.P3D/Services/P3DPlayerMovementCompensationService.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
+using OpenTelemetry.Trace;
+
 using P3D.Legacy.Server.Abstractions;
 using P3D.Legacy.Server.Abstractions.Events;
 using P3D.Legacy.Server.Application.Services;
@@ -24,13 +26,15 @@ namespace P3D.Legacy.Server.Client.P3D.Services
     internal class P3DPlayerMovementCompensationService : BackgroundService
     {
         private readonly ILogger _logger;
+        private readonly Tracer _tracer;
         private readonly IPlayerContainerReader _playerContainer;
         private readonly IEventDispatcher _eventDispatcher;
         private readonly TimeLimiter _timeLimiter;
 
-        public P3DPlayerMovementCompensationService(ILogger<P3DPlayerMovementCompensationService> logger, IPlayerContainerReader playerContainer, IEventDispatcher eventDispatcher)
+        public P3DPlayerMovementCompensationService(ILogger<P3DPlayerMovementCompensationService> logger, TracerProvider traceProvider, IPlayerContainerReader playerContainer, IEventDispatcher eventDispatcher)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _tracer = traceProvider.GetTracer("P3D.Legacy.Server.Client.P3D");
             _playerContainer = playerContainer ?? throw new ArgumentNullException(nameof(playerContainer));
             _eventDispatcher = eventDispatcher ?? throw new ArgumentNullException(nameof(eventDispatcher));
             _timeLimiter = TimeLimiter.GetFromMaxCountByInterval(1, TimeSpan.FromMilliseconds(1000D / 60D));
@@ -38,6 +42,8 @@ namespace P3D.Legacy.Server.Client.P3D.Services
 
         protected override async Task ExecuteAsync(CancellationToken ct)
         {
+            using var span = _tracer.StartActiveSpan("P3D Player Movement Compensation Service");
+
             async Task LoopAction()
             {
                 foreach (var group in _playerContainer.GetAll().OfType<IP3DPlayerState>().GroupBy(static x => x.LevelFile, StringComparer.Ordinal))
